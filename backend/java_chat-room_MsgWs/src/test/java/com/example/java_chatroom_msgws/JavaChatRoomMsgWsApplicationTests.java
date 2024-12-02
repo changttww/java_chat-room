7f321d557e2aded358aa8ba0008565002d372598
Viruatios <140522287+Viruatios@users.noreply.github.com>
2024-12-02 20:43:24 +0800

    websoket

---
+++ backend/java_chat-room_MsgWs/src/test/java/com/example/java_chatroom_msgws/JavaChatRoomMsgWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_chatroom_msgws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChatRoomMsgWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
