976bada21cb1feed86b5b34aae6cc7454f08e9b0
Viruatios <140522287+Viruatios@users.noreply.github.com>
2024-12-13 00:38:44 +0800

    Revert "MaCY‘s"
    This reverts commit 92bd7d19fbd53ed3894c067d70b075d8d2af680b.

--- src/main/java/com/example/chatroom/repository/RoomRepository.java
+++ src/main/java/com/example/chatroom/repository/RoomRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface RoomRepository extends JpaRepository<Room, Integer> {
+public interface RoomRepository extends JpaRepository<Room, String> {
     Optional<Room> findByRoomId(Integer roomId);
     // 查询房间类型是 private
     List<Room> findByRoomType(String roomType);
